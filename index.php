e96ac462debafdef4c36a73ff7ee0377e1dbed74
Anthony <acs.agl46@gmail.com>
2020-11-08 15:18:35 +0100

    install template

--- index.php
+++ index.php
@@ -9,4 +9,16 @@ use Twig\Loader\FilesystemLoader;
 $loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader);
 
+// Connexion BDD
+
+require_once("modele/UserRepository.php");
+require_once("modele/PostRepository.php");
+require_once("modele/CategoryRepository.php");
+require_once("modele/CommentRepository.php");
+
+$postRepo = new PostRepository;
+$userRepo = new UserRepository;
+$catRepo = new CategoryRepository;
+$commentRepo = new CommentRepository;
+
 echo $twig->render('layout.html.twig');
